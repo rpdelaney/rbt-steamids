0c1a96f0c640f60cbb8b1c85cf61039edeae900a
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2020-12-25 12:22:40 -0500

    Put player parsing logic in own function

--- steamids/search.py
+++ steamids/search.py
@@ -3,7 +3,7 @@
 
 import re
 from steam.steamid import SteamID as sid
-from typing import Dict
+from typing import Dict, Union
 import json
 
 bracketed = re.compile("\\[.+\\]")
@@ -27,6 +27,25 @@ def parse_team(line) -> Dict[str, str]:
     }
 
 
+def parse_player(line) -> Dict[str, Union[str, bool]]:
+    # player info
+    words = [
+        word.strip()
+        for word in re.split(r"[\[\]\n]+", line.strip("-"))
+        if word.strip()
+    ]
+    player_name = words[0]
+    steamid64 = words[1]
+
+    return {
+        "name": player_name,
+        "steamid_64": steamid64,
+        "steamid": sid(steamid64).as_steam2,
+        "valid": sid.is_valid(sid(steamid64)),
+        "url": sid(steamid64).community_url,
+    }
+
+
 def main():
     team = {}
     teams = []
@@ -40,24 +59,8 @@ def main():
                 team = parse_team(line)
 
             if line[0] == "-":
-                # player info
-                words = [
-                    word.strip()
-                    for word in re.split(r"[\[\]\n]+", line.strip("-"))
-                    if word.strip()
-                ]
-                player_name = words[0]
-                steamid64 = words[1]
+                team["players"].append(parse_player(line))
 
-                team["players"].append(
-                    {
-                        "name": player_name,
-                        "steamid_64": steamid64,
-                        "steamid": sid(steamid64).as_steam2,
-                        "valid": sid.is_valid(sid(steamid64)),
-                        "url": sid(steamid64).community_url,
-                    }
-                )
     teams.append(team)
 
     print(json.dumps(teams, default=str))
